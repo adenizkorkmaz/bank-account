11b5e10ca37976f6ba964956c40ace41ed16b593
Deniz Korkmaz <deniz.korkmaz@bestseller.com>
2020-05-09 11:33:12 +0200

    cross origin controll added to controller, currency added to account

--- src/main/java/com/example/bank/model/dto/AccountResponseDto.java
+++ src/main/java/com/example/bank/model/dto/AccountResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.bank.model.dto;
 
+import com.example.bank.model.Currency;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +21,8 @@ public class AccountResponseDto extends RepresentationModel<AccountResponseDto> 
     private Long userId;
 
     private BigDecimal balance;
+
+    private Currency currency;
 
     private Boolean current;
 
